e3349d9a51cd1f506aacfc2301c6071a4332fc21
Deepthy R <rkdeepthy@gmail.com>
2017-11-20 22:24:58 -0800

    PrettyPrint

--- level2/Solution.java
+++ level2/Solution.java
@@ -33,17 +33,14 @@ public class Solution {
 
             if(i == layers-1){
                 matrix[i][i] = A;
-
             }else{
                 for(int j = i; j<n-1-i; j++){
                     matrix[i][j] = A;
                     matrix[j][n-1-i] = A;
                     matrix[n-1-i][n-1-j] = A;
                     matrix[n-1-j][i] = A;
-
                 }
             }
-
             A--;
         }
 
@@ -54,13 +51,12 @@ public class Solution {
         Solution sol = new Solution();
 
         int [][] arr = sol.prettyPrint(4);
-         for (int i=0; i<arr.length; i++) {
+        for (int i=0; i<arr.length; i++) {
            for (int j=0; j<arr[i].length; j++) {
-                     System.out.print(arr[i][j]);
+                System.out.print(arr[i][j]);
 
            }
-            System.out.println();
-
+           System.out.println();
          }
     }
 }
